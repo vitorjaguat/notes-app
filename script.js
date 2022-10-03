177f41a9193dcc64457e2ff756d1f4d3a1166188
Vitor Jaguat <vitorbutkus@gmail.com>
2022-10-03 16:28:16 -0300

    add html, css, js

---
+++ script.js
@@ -0,0 +1,75 @@
+const addBtn = document.getElementById('add');
+
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+if(notes) {
+    notes.forEach(note => addNewNote(note));
+}
+
+addBtn.addEventListener('click', () => addNewNote());
+
+function addNewNote(text = '') {
+    const note = document.createElement('div');
+    note.classList.add('note');
+
+    note.innerHTML = `
+    <div class="tools">
+        <button class="edit">
+            <i class="fas fa-edit"></i>
+        </button>
+        <button class="delete">
+            <i class="fas fa-trash-alt"></i>
+        </button>
+    </div>
+
+    <div class="main ${text ? '' : 'hidden'}"></div>
+    <textarea class="${text ? 'hidden' : ''}"></textarea>
+    `;
+
+    const editBtn = note.querySelector('.edit');
+    const deleteBtn = note.querySelector('.delete');
+    const main = note.querySelector('.main');
+    const textArea = note.querySelector('textarea');
+
+    textArea.value = text;
+    main.innerHTML = marked.parse(text);
+    //shows the text fomatted as Markdown
+
+    deleteBtn.addEventListener('click', () => {
+        note.remove();
+        updateLS();
+    })
+
+    editBtn.addEventListener('click', () => {
+        main.classList.toggle('hidden');
+        textArea.classList.toggle('hidden');
+    })
+
+    textArea.addEventListener('input', (e) => {
+        const { value } = e.target;
+        main.innerHTML = marked.parse(value);
+        updateLS();
+    })
+
+    document.body.appendChild(note);
+}
+
+
+//Using localStorage native API:
+
+// localStorage.setItem('key', 'value');
+// localStorage.getItem('key');
+// localStorage.removeItem('key');
+
+// localStorage only stores strings as values. If you want to store an object:
+// localStorage.setItem('key', JSON.stringify({id: 'xxx', name: 'Brad'}));
+// JSON.parse(localStorage.getItem('key'));
+
+// To see stored items on the borwser, go to Inspect / Application / Storage
+
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea');
+    const notes = [];
+    notesText.forEach(note => notes.push(note.value));
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
